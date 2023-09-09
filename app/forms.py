6b918a2e61c18a9d23d3c376db397933247042b4
qd-nguyen <84310665+qd-nguyen@users.noreply.github.com>
2023-09-10 00:51:30 +0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, DateField, SelectField, TextAreaField, SubmitFi
 from wtforms.validators import DataRequired, Length, Optional
 
 class TodoTaskForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(), Length(max=128)])
-    deadline = DateField('Deadline', validators=[Optional()])
-    status = SelectField('Status', choices=[('offen', 'Offen'), ('erledigt', 'Erledigt')], validators=[DataRequired()])
-    comment = TextAreaField('Comment', validators=[Optional(), Length(max=512)])
-    submit = SubmitField('Hinzufügen')
+    title = StringField('Titel', validators=[DataRequired(), Length(max=128)])
+    deadline = DateField('Fälligkeitsdatum', format='%Y-%m-%d', validators=[Optional()])
+    status = SelectField('Status', choices=[('Offen', 'Offen'), ('In Bearbeitung', 'In Bearbeitung'), ('Erledigt', 'Erledigt')], validators=[DataRequired()])
+    comment = TextAreaField('Kommentar', validators=[Optional(), Length(max=512)])
+    submit = SubmitField('Aufgabe hinzufügen')
